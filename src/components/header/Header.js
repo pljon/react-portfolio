605dcfef6235848ed665a279e81988e1bb8fc0dc
Jonathan Plaras <jonplaras@gmail.com>
2023-02-04 00:03:06 -0500

    created components for social, home, data. style file for home. added hand.svg. show/hide menu for mobile/small screen users. imported home component to root app.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,62 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+  // toggle menu
+  const [Toggle, showMenu] = useState(false);
+
   return (
     <header className="header">
-        <nav className="nav container">
-            <a href="index.html" className="nav__logo">Plaras</a>
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Plaras
+        </a>
 
-            <div className="nav__menu">
-                <ul className="nav__list grid">
-                     
-                    <li className="nav__item">
-                        <a href="#home" className="nav__link active-link">
-                            <i className="uil uil-estate nav__icon"></i> Home
-                        </a>
-                    </li>
-                    
-                    <li className="nav__item">
-                        <a href="#about" className="nav__link">
-                            <i className="uil uil-user nav__icon"></i> About
-                        </a>
-                    </li>
+        {/* toggle menu on click event handler and show menu */}
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
 
-                    <li className="nav__item">
-                        <a href="#portfolio" className="nav__link">
-                            <i className="uil uil-scenery nav__icon"></i> Portfolio
-                        </a>
-                    </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
 
-                    <li className="nav__item">
-                        <a href="#placeholder" className="nav__link">
-                            <i className="uil uil-briefcase nav__icon"></i> PLACEHOLDER
-                        </a>
-                    </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
 
-                    <li className="nav__item">
-                        <a href="#placeholder" className="nav__link">
-                            <i className="uil uil-file-alt nav__icon"></i> PLACEHOLDER
-                        </a>
-                    </li>
+            <li className="nav__item">
+              <a href="#placeholder" className="nav__link">
+                <i className="uil uil-briefcase nav__icon"></i> PLACEHOLDER
+              </a>
+            </li>
 
-                    <li className="nav__item">
-                        <a href="#contact" className="nav__link">
-                            <i className="uil uil-message nav__icon"></i> Contact
-                        </a>
-                    </li>
+            <li className="nav__item">
+              <a href="#placeholder" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> PLACEHOLDER
+              </a>
+            </li>
 
-                </ul>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
 
-                <i class="uil uil-times nav__close"></i>
-            </div>
+          <i
+            class="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
 
-            <div className="nav__toggle">
-                <i class="uil uil-apps"></i>
-            </div>
-        </nav>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i class="uil uil-apps"></i>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/home/Social.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="#linkedinplaceholder"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/pljon"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
