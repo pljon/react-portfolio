01378ee28ac36815eb7f9c3b9822fb2a9f3d106d
Jonathan Plaras <jonplaras@gmail.com>
2023-02-15 21:23:45 -0500

    completed footer, added footer styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">pl.jon</h1>
 
-export default Footer
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Portfolio
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://github.com/pljon"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="uil uil-github-alt"></i>
+          </a>
+        </div>
+
+        {/* <span className="footer__copy">pljon All Rights Reserved</span> */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
